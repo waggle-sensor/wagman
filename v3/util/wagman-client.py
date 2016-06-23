ff5eb27d354d0ee89c9f4936500371b6a521dbfe
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-23 13:45:07 -0500

    fix prefix

--- v3/util/wagman-client.py
+++ v3/util/wagman-client.py
@@ -3,6 +3,10 @@
 import sys
 from serial import Serial
 
+
+header_prefix = '<<<-'
+footer_prefix = '->>>'
+
 serial = Serial('/dev/waggle_sysmon', 115200)
 
 command = ' '.join(sys.argv[1:])
@@ -12,7 +16,7 @@ serial.write(b'\n')
 # header
 header = serial.readline().decode().strip()
 
-if header.startswith('<<<-'):
+if header.startswith(header_prefix):
     print "header found"
 else:
     print "header not found"
@@ -23,7 +27,7 @@ else:
 while (1):
     line = serial.readline().decode().strip()
     
-    if line.startswith('<<<-'):
+    if line.startswith(footer_prefix):
         break
         
     print line

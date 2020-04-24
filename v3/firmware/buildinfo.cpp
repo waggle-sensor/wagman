8887958769e2891caa6cfd416cd6cc2cf64503ae
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-24 10:40:27 -0500

    v3 - changed hb timeout to 1h, changed default media to emmc

--- v3/firmware/buildinfo.cpp
+++ v3/firmware/buildinfo.cpp
@@ -1,8 +1,2 @@
-// ANL:waggle-license
-// This file is part of the Waggle Platform.  Please see the file
-// LICENSE.waggle.txt for the legal details of the copyright and software
-// license.  For more details on the Waggle project, visit:
-//          http://www.wa8.gl
-// ANL:waggle-license
-#define BUILD_TIME 1523549574
-#define BUILD_GIT "67b0"
+#define BUILD_TIME 1587742757
+#define BUILD_GIT "a64b494dad76e20a3b1549c1e7a4b1335be548f2"
